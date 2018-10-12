f00005c5e8568be8ce800997c3360ed6a8676ab0
Kyle Andrews <kwandrews7@gmail.com>
2018-10-12 16:20:23 -0500

    Update to where are you

--- scripts/whereami.js
+++ scripts/whereami.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const hostname = os.hostname();
+const hostname = os.hostname().includes('Kyle-Andrews-MacBook-Pro') ? 'Chilling with @kwandrews7 :mustang:' : 'Hanging out in the Cloud :cloud:';
 
 module.exports = robot =>
   robot.respond(/where are you/i, message => message.send(hostname))
