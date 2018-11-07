84758e622478f2097902ce28293650873d6d050f
Kyle Andrews <kwandrews7@gmail.com>
2018-11-07 16:21:23 -0600

    Some fun location updates to where-are-you script.

--- scripts/where-am-i.js
+++ scripts/where-am-i.js
@@ -1,6 +1,19 @@
 const os = require('os');
-const hostname = os.hostname().includes('Kyle-Andrews-MacBook-Pro') ? 'Chilling with @kwandrews7 :mustang:' : 'Hanging out in the Cloud :cloud:';
 
-module.exports = robot =>
-  robot.respond(/where are you/i, message => message.send(hostname))
-;
+function sendServerLoc(msg) {
+  console.log(`where-am-i called`);
+  msg.http(`http://ip-api.com/json`).get()(function (err, res, body) {
+    let _body = JSON.parse(body);
+    if (_body.city && _body.regionName) {
+      msg.send(`${_body.city}, ${_body.regionName} [${os.hostname()}]`);
+    }
+    if (_body.lat && _body.lon) {
+      msg.send(`https://www.google.com/maps/search/?api=1&query=${_body.lat},${_body.lon}`);
+    }
+  });
+}
+
+module.exports = function (robot) {
+  robot.respond(/where are you/i, message => sendServerLoc(message));
+  robot.respond(/where you at/i, message => sendServerLoc(message));
+};
