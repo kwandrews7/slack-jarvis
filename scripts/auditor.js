998a317aa1060c97ac3a44b942d88a1debab1087
Kyle Andrews <kwandrews7@gmail.com>
2018-11-30 18:09:21 -0600

    Updated auditor, add mongo stats.

--- scripts/auditor.js
+++ scripts/auditor.js
@@ -1,11 +1,10 @@
-const Util = require("util");
 const MongoClient = require('mongodb').MongoClient;
 const roomMap = {
-  "C0D2Z96AG": "hubot-testing",
-  "C03AFTJHG": "general",
-  "C4YTEUTJ6": "generaltsos",
-  "CCE6AE5GE": "investing",
-  "CEFH3HW1M": "whatsplaying"
+  'C0D2Z96AG': 'hubot-testing',
+  'C03AFTJHG': 'general',
+  'C4YTEUTJ6': 'generaltsos',
+  'CCE6AE5GE': 'investing',
+  'CEFH3HW1M': 'whatsplaying'
 };
 
 module.exports = function (robot) {
@@ -15,7 +14,7 @@ module.exports = function (robot) {
     },
     function (err, client) {
       if (err) {
-        robot.logger.error("Auditor was unable to connect to slack_messages connection!");
+        robot.logger.error('Auditor was unable to connect to slack_messages connection!');
         robot.logger.error(err);
       }
 
@@ -27,21 +26,21 @@ module.exports = function (robot) {
       robot.hear(/^.*$/, function (msg) {
         let room = msg.message.room;
         if (room.startsWith('G') || room.startsWith('D')) {
-          robot.logger.info("Skipping messages from ignored room.");
+          robot.logger.info('Skipping messages from ignored room.');
           return;
         }
         let savedMessage = {
-          "room": roomMap[room],
-          "room_id": room,
-          "user": msg.message.user.name,
-          "user_id": msg.message.user.id,
-          "message_id": msg.message.id,
-          "text": msg.message.text,
-          "timestamp": Date.now()
+          'room': roomMap[room],
+          'room_id': room,
+          'user': msg.message.user.name,
+          'user_id': msg.message.user.id,
+          'message_id': msg.message.id,
+          'text': msg.message.text,
+          'timestamp': Date.now()
         };
         collection.insertOne(savedMessage, function (err, res) {
           if (err) {
-            robot.logger.error("Auditor was unable to save message!");
+            robot.logger.error('Auditor was unable to save message!');
             robot.logger.error(err);
           }
         });
---
+++ scripts/mongo-stats.js
@@ -0,0 +1,109 @@
+const MongoClient = require('mongodb').MongoClient;
+const roomMap = {
+  'C0D2Z96AG': 'hubot-testing',
+  'C03AFTJHG': 'general',
+  'C4YTEUTJ6': 'generaltsos',
+  'CCE6AE5GE': 'investing',
+  'CEFH3HW1M': 'whatsplaying'
+};
+
+module.exports = function (robot) {
+
+  MongoClient.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true
+    },
+    function (err, client) {
+      if (err) {
+        robot.logger.error('MongoStats was unable to connect to slack_messages connection!');
+        robot.logger.error(err);
+      }
+
+      robot.logger.debug('MongoStats - Successfully authenticated with mongo');
+
+      let db = client.db(process.env.MONGODB_DBNAME);
+      let collection = db.collection('slack_messages');
+
+      robot.respond(/give us the stats/, function (msg) {
+        var content = {};
+
+        let endTime = new Date();
+        endTime.setUTCHours(0, 0, 0);
+        let startTime = new Date(endTime - (7 * 24 * 60 * 60 * 1000));
+        content.start = startTime.toDateString();
+        content.end = endTime.toDateString();
+        let timestampQuery = {
+          $gte: startTime.getTime(),
+          $lte: endTime.getTime()
+        };
+
+        getMessageCount(collection, timestampQuery, function (error, result) {
+          content.messageCount = result || -1;
+          respondWithContent(msg, content);
+        });
+        
+        getRoomsAggregate(collection, timestampQuery, function (error, result) {
+          content.roomCounts = result || [];
+          respondWithContent(msg, content);
+        });
+        
+        getUsersAggregate(collection, timestampQuery, function (error, result) {
+          content.userCounts = result || [];
+          respondWithContent(msg, content);
+        });
+
+      });
+    });
+};
+
+function getUsersAggregate(collection, timestampQuery, callback) {
+  let userPipeline = [{ $match: { timestamp: timestampQuery } },
+    { $group: { _id: "$user", count: { $sum: 1 } } }];
+  collection.aggregate(userPipeline).toArray(callback);
+}
+
+function getRoomsAggregate(collection, timestampQuery, callback) {
+  let roomPipeline = [{ $match: { timestamp: timestampQuery } },
+    { $group: { _id: "$room", count: { $sum: 1 } } }];
+  collection.aggregate(roomPipeline).toArray(callback);
+}
+
+function getMessageCount(collection, timestampQuery, callback) {
+  let query = {
+    timestamp: timestampQuery
+  };
+  collection.countDocuments(query, {}, callback);
+}
+
+function respondWithContent(robot, content) {
+  if (robot && content && content.start &&
+    content.end && content.messageCount &&
+    content.roomCounts && content.userCounts) {
+    robot.send('*Last Week Messages Statistics*');
+    robot.send(`*Timeframe:* _${content.start} - ${content.end}_`);
+    robot.send(`*Total Message Count:* _${content.messageCount}_`);
+    robot.send(`*Stats by User:*`);
+    content.userCounts.sort(compareAggregateResults).forEach(function(user, index) {
+      robot.send(`_${index+1}. ${user._id}: ${user.count} ${optionalMedal(index)}_`);
+    });
+    robot.send(`*Stats by Room:*`);
+    content.roomCounts.sort(compareAggregateResults).forEach(function(room, index) {
+      robot.send(`_${index+1}. ${room._id}: ${room.count} ${optionalMedal(index)}_`);
+    });
+  }
+}
+
+function compareAggregateResults(a, b) {
+  return b.count - a.count;
+}
+
+function optionalMedal(index) {
+  if (index === 0) {
+    return ':gold:';
+  } else if (index === 1) {
+    return ':silver:';
+  } else if (index === 2) {
+    return ':bronze:';
+  } else {
+    return '';
+  }
+}
