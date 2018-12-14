07cef162d218c606b9a49e2bea107533ebcdd030
Kyle Andrews <kwandrews7@gmail.com>
2018-12-14 14:45:42 -0600

    Add more configurable stats commands.

--- scripts/mongo-stats.js
+++ scripts/mongo-stats.js
@@ -1,3 +1,8 @@
+// Commands:
+//   jarvis weekly stats (yyyy-mm-dd) - Provide weekly stats. (Date optional)
+//   jarvis daily stats (yyyy-mm-dd) - Provide daily stats. (Date optional)
+
+
 const MongoClient = require('mongodb').MongoClient;
 const roomMap = {
   'C0D2Z96AG': 'hubot-testing',
@@ -23,14 +28,14 @@ module.exports = function (robot) {
       let db = client.db(process.env.MONGODB_DBNAME);
       let collection = db.collection('slack_messages');
 
-      robot.respond(/(give us the stats|what are the stats)/i, function (msg) {
+      robot.respond(/(give us the stats|weekly stats)( )?([0-9]{4}-[0-9]{2}-[0-9]{2})?/i, function (msg) {
         var content = {};
 
-        let endTime = new Date();
-        endTime.setUTCHours(0, 0, 0);
+        let dateStr = msg.match[3] || new Date().toISOString().substr(0, 10);
+        let endTime = new Date(dateStr + 'T00:00:00+00:00');
         let startTime = new Date(endTime - (7 * 24 * 60 * 60 * 1000));
-        content.start = startTime.toDateString();
-        content.end = endTime.toDateString();
+        content.start = startTime.toUTCString();
+        content.end = endTime.toUTCString();
         let timestampQuery = {
           $gte: startTime.getTime(),
           $lte: endTime.getTime()
@@ -40,30 +45,79 @@ module.exports = function (robot) {
           content.messageCount = result || -1;
           respondWithContent(msg, content);
         });
-        
+
         getRoomsAggregate(collection, timestampQuery, function (error, result) {
           content.roomCounts = result || [];
           respondWithContent(msg, content);
         });
-        
+
         getUsersAggregate(collection, timestampQuery, function (error, result) {
           content.userCounts = result || [];
           respondWithContent(msg, content);
         });
-
       });
+
+
+      robot.respond(/daily stats( )?([0-9]{4}-[0-9]{2}-[0-9]{2})?/i, function (msg) {
+        let content = {};
+        content.roomCounts = [];
+        
+        let dateStr = msg.match[2] || new Date().toISOString().substr(0, 10);
+        let startTime = new Date(dateStr + 'T00:00:00+00:00');
+        let endTime = new Date(dateStr + 'T23:59:59+00:00');
+        
+        content.start = startTime.toUTCString();
+        content.end = endTime.toUTCString();
+        let timestampQuery = {
+          $gte: startTime.getTime(),
+          $lte: endTime.getTime()
+        };
+
+        getMessageCount(collection, timestampQuery, function (error, result) {
+          content.messageCount = result || -1;
+          respondWithContent(msg, content);
+        });     
+
+        getUsersAggregate(collection, timestampQuery, function (error, result) {
+          content.userCounts = result || [];
+          respondWithContent(msg, content);
+        });        
+      });
+
     });
 };
 
 function getUsersAggregate(collection, timestampQuery, callback) {
-  let userPipeline = [{ $match: { timestamp: timestampQuery } },
-    { $group: { _id: "$user", count: { $sum: 1 } } }];
+  let userPipeline = [{
+      $match: {
+        timestamp: timestampQuery
+      }
+    },
+    {
+      $group: {
+        _id: "$user",
+        count: {
+          $sum: 1
+        }
+      }
+    }];
   collection.aggregate(userPipeline).toArray(callback);
 }
 
 function getRoomsAggregate(collection, timestampQuery, callback) {
-  let roomPipeline = [{ $match: { timestamp: timestampQuery } },
-    { $group: { _id: "$room", count: { $sum: 1 } } }];
+  let roomPipeline = [{
+      $match: {
+        timestamp: timestampQuery
+      }
+    },
+    {
+      $group: {
+        _id: "$room",
+        count: {
+          $sum: 1
+        }
+      }
+    }];
   collection.aggregate(roomPipeline).toArray(callback);
 }
 
@@ -79,17 +133,21 @@ function respondWithContent(robot, content) {
     content.end && content.messageCount &&
     content.roomCounts && content.userCounts) {
     var response = [];
-    response.push('*Last Week Messages Statistics*');
-    response.push(`*Timeframe:* _${content.start} - ${content.end}_`);
+    response.push('*Messages Statistics*');
+    response.push(`_${content.start} - ${content.end}_`);
     response.push(`*Total Message Count:* _${content.messageCount}_`);
-    response.push(`*Stats by User:*`);
-    content.userCounts.sort(compareAggregateResults).forEach(function(user, index) {
-      response.push(`_${index+1}. ${user._id}: ${user.count} ${optionalMedal(index)}_`);
-    });
-    response.push(`*Stats by Room:*`);
-    content.roomCounts.sort(compareAggregateResults).forEach(function(room, index) {
-      response.push(`_${index+1}. ${room._id}: ${room.count} ${optionalMedal(index)}_`);
-    });
+    if (content.userCounts.length > 0) {
+      response.push(`*Stats by User:*`);
+      content.userCounts.sort(compareAggregateResults).forEach(function (user, index) {
+        response.push(`_${index+1}. ${user._id}: ${user.count} ${optionalMedal(index)}_`);
+      });
+    }
+    if (content.roomCounts.length > 0) {
+      response.push(`*Stats by Room:*`);
+      content.roomCounts.sort(compareAggregateResults).forEach(function (room, index) {
+        response.push(`_${index+1}. ${room._id}: ${room.count} ${optionalMedal(index)}_`);
+      }); 
+    }
     robot.send(response.join('\n'));
   }
 }
